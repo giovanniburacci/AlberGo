22e12771f0e0acba5c8c83134957fa0d16b92327
Giovanni Buracci <giovanni.buracci@hotmail.com>
2021-11-24 00:42:32 +0100

    fixed buttons in menu

--- src/containers/menuContainer/menuContainer.container.tsx
+++ src/containers/menuContainer/menuContainer.container.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react';
+import React, {ReactNode, useEffect} from 'react';
 import {
     ContactsOutlined,
     ControlOutlined,
@@ -11,9 +11,14 @@ import './menuContainer.scss'
 import MenuItem from 'antd/es/menu/MenuItem';
 import {Tag} from 'antd';
 import {useHistory} from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import categorieSelector from '../../store/categorie/categorie.selector';
 import stanzeSelector from '../../store/stanze/stanze.selector';
+import categorieActions from '../../store/categorie/categorie.action';
+import {loginSelector} from '../../store/login/login.selector';
+import clientiSelector from '../../store/clienti/clienti.selector';
+import clientiActions from '../../store/clienti/clienti.action';
+import stanzeActions from '../../store/stanze/stanze.action';
 
 interface Option {
     title: String,
@@ -28,6 +33,7 @@ interface MenuContainerProps {
     isAdmin: boolean
 }
 
+let renderCount = 0;
 export const MenuContainer = (props:MenuContainerProps) => {
     const {isCollapsed, isAdmin} = props;
     const history = useHistory();
@@ -67,10 +73,23 @@ export const MenuContainer = (props:MenuContainerProps) => {
             path: '/fatture'
         }
     ];
+    const dispatch = useDispatch();
 
     //todo ignazio mi serve il count
     const categorie = useSelector(categorieSelector.getCategorie);
     const stanze = useSelector(stanzeSelector.getStanze)
+    const hotelId = useSelector(loginSelector.getIdHotel)
+    const clienti = useSelector(clientiSelector.getClienti)
+    useEffect(() => {
+        if(renderCount === 0) {
+            if(hotelId) {
+                dispatch(categorieActions.fetchCategorie(hotelId));
+                dispatch(clientiActions.fetchClienti(hotelId))
+                dispatch(stanzeActions.fetchStanze(hotelId))
+                renderCount ++;
+            }
+        }
+    }, [hotelId])
 
     return (
         <div className={`${componentClassName}`}>
@@ -103,25 +122,37 @@ export const MenuContainer = (props:MenuContainerProps) => {
                              style={{backgroundImage: 'linear-gradient( 135deg, #70F570 10%, #49C628 100%)',
                                  color: '#ffffff'}}
                              className={!isCollapsed ? `${componentClassName}__badges__tag green` : `${componentClassName}__badges__tag__collapsed green`}>
-                            {stanze && stanze.length} {!isCollapsed && <>Stanze totali</>}
+
+                            {stanze && stanze.length}
+
+                            {!isCollapsed && <>Stanze totali</>}
                         </Tag>
                         <Tag color={'default'}
                              style={{backgroundImage: 'linear-gradient( 135deg, #F05F57 10%, #360940 100%)',
                                  color: '#ffffff'}}
                              className={!isCollapsed ? `${componentClassName}__badges__tag` : `${componentClassName}__badges__tag__collapsed`}>
-                            X {!isCollapsed && <>Stanze occupate</>}
+
+                            {stanze && stanze.filter(s => s.fuoriServizio).length}
+
+                            {!isCollapsed && <>Stanze fuori servizio</>}
                         </Tag>
                         <Tag color={'default'}
                              style={{backgroundImage: 'linear-gradient( 135deg, #FCCF31 10%, #F55555 100%)',
                                  color: '#ffffff'}}
                              className={!isCollapsed ? `${componentClassName}__badges__tag` : `${componentClassName}__badges__tag__collapsed`}>
-                            {categorie && categorie.length} {!isCollapsed && <>Categorie</>}
+
+                            {categorie && categorie.length}
+
+                            {!isCollapsed && <>Categorie</>}
                         </Tag>
                         <Tag color={'default'}
                              className={!isCollapsed ? `${componentClassName}__badges__tag` : `${componentClassName}__badges__tag__collapsed`}
                              style={{backgroundImage: 'linear-gradient( 135deg, #3B2667 10%, #BC78EC 100%)',
                                  color: '#ffffff'}}>
-                            X {!isCollapsed && <>Clienti registrati</>}
+
+                            {clienti && clienti.length}
+
+                            {!isCollapsed && <>Clienti registrati</>}
                         </Tag>
                     </div>
                 )
